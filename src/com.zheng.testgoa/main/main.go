6c01eee201365936d9ab961844c833d7b16d6066
zhengchentong <zhengchentong@xdf.cn>
2020-11-16 20:30:44 +0800

    file

--- src/com.zheng.testgoa/main/main.go
+++ src/com.zheng.testgoa/main/main.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	newFile *os.File
+	err     error
+)
 
 func main() {
 
-	fmt.Println("a")
+	fmt.Println("a1")
+
+	newFile, err := os.Open("/Users/zct/Desktop/baocuo.txt")
+	if err!=nil{
+		log.Fatal(err)
+	}
+	data, err1 := ioutil.ReadAll(newFile)
+	if err1 !=nil{
+		log.Fatal(err1)
+	}
+	fmt.Println(string(data))
+
+	newFile.Close()
 }
