395240229fc546eb20b47f800f315ebb6940c728
zhengchentong <zhengchentong@xdf.cn>
2020-11-18 17:37:59 +0800

    gorm

--- bin/main.go
+++ bin/main.go
@@ -1,15 +1,61 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+type test struct {
+	Id int
+	A  string
+	B  string
+}
+
+// UserInfo 用户信息
+type UserInfo struct {
+	ID     uint
+	Name   string
+	Gender string
+	Hobby  string
+}
 
 func main() {
 	router := gin.Default()
 	router.GET("/hello", func(context *gin.Context) {
-		context.JSON(200,gin.H{
-			"code":200,
-			"success":true,
+		db, err := gorm.Open("mysql", "root:@(localhost:3306)/test?charset=utf8mb4&parseTime=True&loc=Local")
+		db.LogMode(true)
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		var test1 = new(test)
+		find := db.Table("test").First(test1)
+		fmt.Println(find)
+		//fmt.Println(len(list))
+		//fmt.Println(list)
+		//for i := range list {
+		//	t := list[i]
+		//	fmt.Println(t.a)
+		//	println(t.b)
+		//}
+
+		//db.AutoMigrate(&UserInfo{})
+		//u1 := UserInfo{1, "七米", "男", "篮球"}
+		//u2 := UserInfo{2, "沙河娜扎", "女", "足球"}
+		//// 创建记录
+		//db.Create(&u1)
+		//db.Create(&u2)
+		//// 查询
+		//var u = new(UserInfo)
+		//db.First(u)
+		//fmt.Printf("%#v\n", u)
+
+		context.JSON(200, gin.H{
+			"code":    200,
+			"success": true,
 		})
 	})
 	_ = router.Run("localhost:9090")
