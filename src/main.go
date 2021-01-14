17c61d888a2260003995d3a5a6cb75c32c8a15e4
zhengchentong <zhengchentong@xdf.cn>
2021-01-14 16:47:11 +0800

    go gin

--- src/main.go
+++ src/main.go
@@ -7,9 +7,9 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/hello", func(context *gin.Context) {
-		context.JSON(200,gin.H{
-			"code":200,
-			"success":true,
+		context.JSON(200, gin.H{
+			"code":    200,
+			"success": true,
 		})
 	})
 	_ = router.Run("localhost:9090")
